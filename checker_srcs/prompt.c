4ef58db0545f39157560c7c2eea0964d68d524c9
Alexandre Hallaine <hallainealexandre@gmail.com>
2021-03-08 11:40:02 +0100

    update: print

--- checker_srcs/prompt.c
+++ checker_srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: ahallain <ahallain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 15:37:59 by ahallain          #+#    #+#             */
-/*   Updated: 2021/03/07 20:43:26 by ahallain         ###   ########.fr       */
+/*   Updated: 2021/03/08 11:17:38 by ahallain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@
 #include "../utils/lib.h"
 #include "../operations/operations.h"
 
-void	printnumber(t_number number)
+void	printnumber(t_number number, char *str, size_t count)
 {
 	printf("\e[H\e[2J");
-	printf("%11s   %s\n", "Stack A", "Stack B");
+	printf("%-8s%7s   %s %8zu\n", str, "Stack A", "Stack B", count);
 	while (number.a || number.b)
 	{
 		if (number.a)
 		{
-			printf("%11d | ", number.a->data);
+			printf("%15d | ", number.a->data);
 			number.a = number.a->next;
 		}
 		else
-			printf("%11s | ", "");
+			printf("%15s | ", "");
 		if (number.b)
 		{
 			printf("%d", number.b->data);
@@ -45,9 +45,11 @@ void	prompt(t_number *number, bool print)
 {
 	int		ret;
 	char	*line;
+	size_t	count;
 
+	count = 0;
 	if (print)
-		printnumber(*number);
+		printnumber(*number, "", count);
 	while (1)
 	{
 		ret = get_next_line(0, &line);
@@ -55,7 +57,7 @@ void	prompt(t_number *number, bool print)
 		{
 			dispatch(number, line);
 			if (print)
-				printnumber(*number);
+				printnumber(*number, line, count++);
 		}
 		free(line);
 		if (ret <= 0)
